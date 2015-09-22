0fdb61b462c6551ac16ea87e61178ab0e0e0e9f7
Saliya Ekanayake <esaliya@gmail.com>
2015-09-22 10:54:53 -0400

    removed slightly different but mostly duplicate functions on readRowRange

--- src/main/java/edu/indiana/soic/spidal/common/BinaryReader2D.java
+++ src/main/java/edu/indiana/soic/spidal/common/BinaryReader2D.java
@@ -45,42 +45,6 @@ public class BinaryReader2D {
         return null;
     }
 
-    public static short[][] readRowRange(String fname, Range rows, int globalColCount, ByteOrder endianness, boolean divideByShortMax, double transform){
-        try (FileChannel fc = (FileChannel) Files
-            .newByteChannel(Paths.get(fname), StandardOpenOption.READ)) {
-            int dataTypeSize = Short.BYTES;
-            long pos = ((long) rows.getStartIndex()) * globalColCount *
-                       dataTypeSize;
-            MappedByteBuffer mappedBytes = fc.map(
-                FileChannel.MapMode.READ_ONLY, pos,
-                rows.getLength() * globalColCount * dataTypeSize);
-            mappedBytes.order(endianness);
-
-            int rowCount = rows.getLength();
-            short[][] rowBlock = new short[rowCount][];
-            double tmp;
-            for (int i = 0; i < rowCount; ++i){
-                short [] rowBlockRow = rowBlock[i] = new short[globalColCount];;
-                for (int j = 0; j < globalColCount; ++j){
-                    int procLocalPnum =  i * globalColCount + j;
-                    int bytePosition = procLocalPnum * dataTypeSize;
-                    tmp = mappedBytes.getShort(bytePosition) * (divideByShortMax ? 1.0/Short.MAX_VALUE : 1.0);
-                    // -1.0 indicates missing values
-                    assert tmp == -1.0 || (tmp >= 0.0 && tmp <= 1.0);
-                    if (transform != 1.0 && tmp >= 0.0) {
-                        tmp = Math.pow(tmp, transform);
-                    }
-                    rowBlockRow[j] = (short)(tmp * Short.MAX_VALUE);
-                }
-            }
-           return rowBlock;
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
     public static short[][] readConstant(Range rows, int globalColCount, double w){
         assert w >= 0.0 && w <= 1.0;
         int rowCount = rows.getLength();
