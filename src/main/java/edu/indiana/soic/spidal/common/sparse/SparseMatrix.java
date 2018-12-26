c53120b3280b4d7618089d577b2c3ef70832d735
pulasthi <pulasthi911@gmail.com>
2018-12-26 16:17:45 -0500

    adding sparse matrix matrix multiply

--- src/main/java/edu/indiana/soic/spidal/common/sparse/SparseMatrix.java
+++ src/main/java/edu/indiana/soic/spidal/common/sparse/SparseMatrix.java
@@ -8,12 +8,63 @@ public class SparseMatrix {
     private final double[] values;
     private final int[] columns;
     private final int[] rowPointers;
+    /**
+     * keeps the diagonal values of the matrix if applicable;
+     */
+    private final double[] diagonal;
 
+    private boolean hasDiagonal;
 
     public SparseMatrix(double[] values, int[] columns, int[] rowPointers) {
         this.rowPointers = rowPointers;
         this.values = values;
         this.columns = columns;
+        this.diagonal = null;
+        this.hasDiagonal = false;
+
+    }
+
+    public SparseMatrix(double[] values, int[] columns, int[] rowPointers,
+                        double[] diagonal) {
+        this.rowPointers = rowPointers;
+        this.values = values;
+        this.columns = columns;
+        this.diagonal = diagonal;
+        this.hasDiagonal = true;
+
+    }
+
+    public SparseMatrix(int values, int columns, int rowPointers) {
+        this.values = new double[values];
+        this.columns = new int[columns];
+        this.rowPointers = new int[rowPointers];
+        this.diagonal = null;
+        this.hasDiagonal = false;
+    }
+
+    public double[] getDiagonal() {
+        return diagonal;
+    }
+
+    public boolean isHasDiagonal() {
+        return hasDiagonal;
+    }
+
+    public void setHasDiagonal(boolean hasDiagonal) {
+        this.hasDiagonal = hasDiagonal;
+    }
+
+    public SparseMatrix(int values, int columns, int rowPointers,
+                        int diagonals) {
+
+        if(rowPointers != diagonals) throw new IllegalStateException("The " +
+                "diagonal must be of size rows");
+
+        this.values = new double[values];
+        this.columns = new int[columns];
+        this.rowPointers = new int[rowPointers];
+        this.diagonal = new double[diagonals];
+        this.hasDiagonal = true;
     }
 
     public double[] getValues() {
--- src/main/java/edu/indiana/soic/spidal/common/sparse/SparseMatrixUtils.java
+++ src/main/java/edu/indiana/soic/spidal/common/sparse/SparseMatrixUtils.java
@@ -2,4 +2,39 @@ package edu.indiana.soic.spidal.common.sparse;
 
 public class SparseMatrixUtils {
 
+    public static void sparseMatrixMatrixMultiply(SparseMatrix sparseMatrix,
+                                             double[] B, int N, double[] out) {
+        double[] values = sparseMatrix.getValues();
+        double[] mutiples = new double[values.length];
+        int[] columns = sparseMatrix.getColumns();
+        int[] rowPointers = sparseMatrix.getRowPointers();
+        int bOffSet = 0;
+
+        for(int dimension = 0; dimension < N; dimension++){
+            bOffSet = N*dimension;
+            // fill the multiples
+            for (int i = 0; i < mutiples.length; i++) {
+                int colIndex = columns[i];
+                mutiples[i] = B[bOffSet + colIndex];
+            }
+
+            int trackIndex = 0;
+            for (int localRow = 0; localRow < rowPointers.length; localRow++) {
+                int rowPointer = rowPointers[localRow];
+                int colCount = (localRow == rowPointers.length - 1) ?
+                        values.length - rowPointer
+                        : rowPointers[localRow + 1] - rowPointer;
+
+                double tempSum = 0;
+                for (int colC = 0; colC < colCount; colC++) {
+                    tempSum += values[trackIndex]*mutiples[trackIndex];
+                    trackIndex++;
+                }
+                out[bOffSet + localRow] = tempSum;
+
+            }
+
+
+        }
+    }
 }
